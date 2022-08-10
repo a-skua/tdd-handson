822b7d65a81c842db6ed62d9dc600b29c30e760c
asuka y <yagi.asuka.0x06@gmail.com>
2022-08-10 09:01:30 +0900

    fix example 3rd step

--- example/lib.go
+++ example/lib.go
@@ -3,3 +3,7 @@ package example
 func Add(a, b int) int {
 	return a + b
 }
+
+func Add3(a, b, c int) int {
+	return 0
+}
--- example/lib_test.go
+++ example/lib_test.go
@@ -10,3 +10,10 @@ func TestAdd(t *testing.T) {
 		t.Fatalf("want=%d, got=%d", 7, result)
 	}
 }
+
+func TestAdd3(t *testing.T) {
+	result := Add3(3, 4, 5)
+	if result != 12 {
+		t.Fatalf("want=%d, got=%d", 12, result)
+	}
+}
