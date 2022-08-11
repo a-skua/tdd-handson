8b94af338b1a127dc6baac07c438b5e013312c17
asuka y <yagi.asuka.0x06@gmail.com>
2022-08-11 16:45:37 +0900

    fix doc/vol01

--- example/lib.go
+++ example/lib.go
@@ -1,9 +1,15 @@
 package example
 
+import (
+	"fmt"
+)
+
+// 2つの整数の和を返す
 func Add(a, b int) int {
 	return sum(a, b)
 }
 
+// 3つの整数の和を返す
 func Add3(a, b, c int) int {
 	return sum(a, b, c)
 }
@@ -13,4 +19,10 @@ func sum(ns ...int) (result int) {
 		result += n
 	}
 	return
+}
+
+// 新しいユーザーIDを発行する
+// ユーザーIDは数字10桁で、数字はランダムに生成される
+func NewUserID(rnd func() int) string {
+	return fmt.Sprintf("%010d", rnd()%10000000000)
 }
--- example/lib_test.go
+++ example/lib_test.go
@@ -1,6 +1,7 @@
 package example
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -15,5 +16,22 @@ func TestAdd3(t *testing.T) {
 	result := Add3(3, 4, 5)
 	if result != 12 {
 		t.Fatalf("want=%d, got=%d", 12, result)
+	}
+}
+
+func TestNewUserID(t *testing.T) {
+	// usage
+	t.Log(NewUserID(rand.Int))
+
+	result := NewUserID(func() int { return 1234567890 })
+	want := "1234567890"
+	if result != want {
+		t.Errorf("result=%s, want=%s", result, want)
+	}
+
+	result = NewUserID(func() int { return 123456789 })
+	want = "0123456789"
+	if result != want {
+		t.Errorf("result=%s, want=%s", result, want)
 	}
 }
